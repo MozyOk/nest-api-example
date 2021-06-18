fca055b456b51902b68bd51e997d24e9a6b63cb1
mozy okubo <mozy.okubo@gmail.com>
2021-06-19 02:01:17 +0900

    add example api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return 'GetUsers Success!';
+  }
+
+  @Get('/:id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return `getUserById Success! Params [id:${id}]`;
+  }
+
+  @Post()
+  createUser(@Body('name') name: string, @Body('desc') desc: string) {
+    return `createUser Success! Params [name:${name}, desc:${desc}]`;
+  }
+
+  @Delete('/:id')
+  deleteUsers(@Param('id', ParseIntPipe) id: number) {
+    return `deleteUsers Success! Params [id:${id}s]`;
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status') status: string,
+  ) {
+    return `updateUser Success! Params [id:${id}, status:${status}]`;
+  }
+}
