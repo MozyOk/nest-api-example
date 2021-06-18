b20f29ab1e15c6fc3009fc5f7ee39bd8d1ad7888
mozy okubo <mozy.okubo@gmail.com>
2021-06-19 02:21:28 +0900

    add mysql setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: 3306,
+      database: 'nest_api_development',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+    }),
+  ],
 })
 export class AppModule {}
